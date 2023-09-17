57bae5c219d08e8229c8f237d7093b9cdb4fc97f
krash6 <145274671+krash6@users.noreply.github.com>
2023-09-17 18:01:33 +0000

    listado

---
+++ Modulo1/scripts/salida de datos .py
@@ -0,0 +1,9 @@
+# Declaramos las variables para almacenar los números
+numero_1 = int(input("Introduce el primer número: "))
+numero_2 = int(input("Introduce el segundo número: "))
+
+# Calculamos la suma de los números
+suma = numero_1 + numero_2
+
+# Imprimimos el resultado
+print("La suma de los números es:", suma)
